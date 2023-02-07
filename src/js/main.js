4f0bb351dbf124939f9b7cee8cfa16aba2ee915e
Melvin Kempe <k3mp314@gmail.com>
2023-02-07 14:46:16 +0100

    [updated] menu slide in

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,13 @@
 import gsap from "gsap"; 
 
 /* eslint-disable no-undef */
-let width = window.innerWidth; 
-
-
 /* varabiler */
 
 const headerH1 = document.querySelector(".h1-header-desktop");
+
 let menuIsOpen = false;
 let menuOverlay = false;
+let width = window.innerWidth;
  
 function init() {
     menuOverlay = document.querySelector(".menu-overlay");
@@ -40,10 +39,10 @@ function createEventListeners() {
     document.querySelector(".menuToggleBtn").addEventListener("click", () => {
         if (!menuIsOpen) { // Mobile menu button handler
             openMenu();
-            gsap.to(menuOverlay, {x: 0});
+            gsap.to(menuOverlay, {x: 0, duration: 1});
         } else {
             closeMenu();
-            //gsap.to(menuOverlay, {x: 20000});
+            gsap.to(menuOverlay, {x: width, duration: 1});
         }
     });
     for (let i = 0; i < menuOptions.length; i++) // Closes menu after a link press
@@ -54,7 +53,6 @@ function createEventListeners() {
 
 
 function closeMenu() {
-    gsap.to(menuOverlay, {opacity:1, x: 50000, display:"grid"}, {opacity:0, x: 500, duration: 0.5, display:"none"});
     gsap.to(".second-row>span:nth-child(1)", {x:0, y:0, duration: 0.15});
     gsap.to(".third-row>span:nth-child(2)", {x:0, y:0, duration: 0.15, delay: 0.1});
     gsap.to(".second-row>span:nth-child(3)", {x:0, y:0, duration: 0.15, delay: 0.2});
@@ -62,7 +60,6 @@ function closeMenu() {
     menuIsOpen = false;
 }
 function openMenu() {
-    gsap.from(menuOverlay, {display:"none", opacity:0, x: 50000},{display:"grid",opacity:1, x: 0, duration: 0.3});
     gsap.to(".first-row>span:nth-child(2)", {x:8.6, y:8.6, duration: 0.15});
     gsap.to(".second-row>span:nth-child(3)", {x:-8.6, y:8.6, duration: 0.15, delay: 0.1});
     gsap.to(".third-row>span:nth-child(2)", {x:-8.6, y:-8.6, duration: 0.15, delay: 0.2});
