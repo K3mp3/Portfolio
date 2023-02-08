e1adcf79ea79d117380b44ca6525e559bf640bdf
Melvin Kempe <k3mp314@gmail.com>
2023-02-08 16:04:46 +0100

    [fixed] bug

--- src/js/threejs.js
+++ src/js/threejs.js
@@ -55,11 +55,11 @@ scene.add(light);
 
 //Camera
 const camera = new THREE.PerspectiveCamera(
-  25, 
-  sizes.width / sizes.height, 
-  0.1, 
-  100
-)
+    25, 
+    sizes.width / sizes.height, 
+    0.1, 
+    100
+);
 camera.position.x = 0
 camera.position.y = 0
 camera.position.z = 4
