b7e020425f2f24c4ddd6478bf4cd48f1d37b690b
Melvin Kempe <k3mp314@gmail.com>
2023-02-27 15:35:35 +0100

    [added] code to move all my project to an array

---
+++ src/js/projects.js
@@ -0,0 +1,11 @@
+const projects = [
+    {
+        images: [
+            {
+                img: "./public/img/nothingPhone.png",
+                alt: "The project which the link goes to",
+            }
+        ],
+        link: "https://medieinstitutet.github.io/fed22d-grafiska-verktyg-gazpachon/",
+    }
+];
