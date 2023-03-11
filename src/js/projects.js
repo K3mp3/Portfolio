5d5fc119a71ac983d8cb92d45b5ed695e5e21e66
Melvin Kempe <k3mp314@gmail.com>
2023-03-11 15:26:56 +0100

    [Edited] all projects moved from HTML to JS

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,3 +1,6 @@
+/* Global variables */
+const myProjectsContainer = document.querySelector(".my-projects-container");
+
 const projects = [
     {
         images: [
@@ -7,5 +10,31 @@ const projects = [
             }
         ],
         link: "https://medieinstitutet.github.io/fed22d-grafiska-verktyg-gazpachon/",
+    },
+    {
+        images: [
+            {
+                img: "./public/img/pizzaleria.jpg",
+                alt: "The project which the link goes to",
+            }
+        ],
+        link: "https://medieinstitutet.github.io/fed22d-grafiska-verktyg-korsbarstomaterna/",
+    },
+    {
+        images: [
+            {
+                img: "./public/img/toTheMoon.jpg",
+                alt: "The project which the link goes to",
+            }
+        ],
+        link: "https://k3mp3.github.io/Major-Tom/",
     }
 ];
+
+for (let i = 0; i < projects.length; i++) {
+    myProjectsContainer.innerHTML += `
+    <div class="project-container">
+    <a target="_blank" href="${projects[i].link}"><img src="${projects[i].images[0].img}"></a>
+    </div>
+`;
+}
